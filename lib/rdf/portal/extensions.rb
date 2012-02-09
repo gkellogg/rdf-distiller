b161f1bea366ccbcc4302c638fe836b45cd527fd
Gregg Kellogg <gregg@kellogg-assoc.com>
2012-02-09 07:01:25 -0800

    Remove Patron, for now, due to issues at hosting provider.

--- lib/rdf/portal/extensions.rb
+++ lib/rdf/portal/extensions.rb
@@ -1,5 +1,4 @@
 require 'rdf/util/file'
-require 'patron'
 
 module RDF::Util
   module File
@@ -17,36 +16,36 @@ module RDF::Util
       when /^file:/
         path = filename_or_url[5..-1]
         Kernel.open(path.to_s, &block)
-      when /^http/
-        headers = {
-          "Accept" => 'text/turtle, application/rdf+xml;q=0.8, text/plain;q=0.4, */*;q=0.1',
-          #"User-Agent" => "Ruby-RDF-Distiller/#{RDF::Portal::VERSION}"
-        }.merge(options[:headers] || {})
-        sess = Patron::Session.new
-        sess.timeout = 30
-        sess.headers['User-Agent'] = 
-        sess.headers['Accept'] = [options[:accept] || 'application/rdf+xml, */*;q=0.1'].flatten.join(', ')
-        resp = sess.get(filename_or_url)
-        io_obj = StringIO.new(resp.body)
-        io_obj.instance_variable_set(:@resp, resp)
-        def io_obj.content_type
-          [@resp.headers["Content-Type"]].flatten.first.split(/[;,]/).first
-        end
-        def io_obj.status
-          @resp.status
-        end
-        def io_obj.charset
-          @resp.charset
-        end
-        if block_given?
-          begin
-            block.call(io_obj)
-          ensure
-            io_obj.close
-          end
-        else
-          io_obj
-        end
+      #when /^http/
+      #  headers = {
+      #    "Accept" => 'text/turtle, application/rdf+xml;q=0.8, text/plain;q=0.4, */*;q=0.1',
+      #    #"User-Agent" => "Ruby-RDF-Distiller/#{RDF::Portal::VERSION}"
+      #  }.merge(options[:headers] || {})
+      #  sess = Patron::Session.new
+      #  sess.timeout = 30
+      #  sess.headers['User-Agent'] = 
+      #  sess.headers['Accept'] = [options[:accept] || 'application/rdf+xml, */*;q=0.1'].flatten.join(', ')
+      #  resp = sess.get(filename_or_url)
+      #  io_obj = StringIO.new(resp.body)
+      #  io_obj.instance_variable_set(:@resp, resp)
+      #  def io_obj.content_type
+      #    [@resp.headers["Content-Type"]].flatten.first.split(/[;,]/).first
+      #  end
+      #  def io_obj.status
+      #    @resp.status
+      #  end
+      #  def io_obj.charset
+      #    @resp.charset
+      #  end
+      #  if block_given?
+      #    begin
+      #      block.call(io_obj)
+      #    ensure
+      #      io_obj.close
+      #    end
+      #  else
+      #    io_obj
+      #  end
       else
         Kernel.open(filename_or_url.to_s, &block)
       end
