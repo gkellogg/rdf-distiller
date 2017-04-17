391dc4e9085bf5f550b5b190263471cbef0ecd81
Gregg Kellogg <gregg@greggkellogg.net>
2017-04-16 17:53:37 -0700

    Update Haml template for RDFa generation to be legal HTML5.

--- lib/rdf/distiller/rdfa_template.rb
+++ lib/rdf/distiller/rdfa_template.rb
@@ -1,23 +1,23 @@
 # Default HAML templates used for generating output from the writer
 module RDF::Distiller
   DISTILLER_HAML = {
-    :identifier => "distiller", 
+    identifier: "distiller", 
     # Document
     # Locals: language, title, profile, prefix, base, subjects
     # Yield: subjects.each
-    :doc => %q(
+    doc: %q(
       !!! XML
       !!! 5
-      %html{:xmlns => "http://www.w3.org/1999/xhtml", :lang => lang, :prefix => prefix}
+      %html{xmlns: "http://www.w3.org/1999/xhtml", lang: lang, prefix: prefix}
         - if base || title
           %head
             - if base
-              %base{:href => base}
+              %base{href: base}
             - if title
               %title= title
-            %link{:rel => "stylesheet", :href => "http://rdf.kellogg-assoc.com/css/distiller.css", :type => "text/css"}
-            %script{:src => "https://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js", :type => "text/javascript"}
-            %script{:src => "http://rdf.kellogg-assoc.com/js/distiller.js", :type => "text/javascript"}
+            %link{rel: "stylesheet", href: "http://rdf.kellogg-assoc.com/css/distiller.css", type: "text/css"}
+            %script{src: "https://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js", type: "text/javascript"}
+            %script{src: "http://rdf.kellogg-assoc.com/js/distiller.js", type: "text/javascript"}
         %body
           - if base
             %p= "RDFa serialization URI base: &lt;#{base}&gt;"
@@ -37,16 +37,23 @@ module RDF::Distiller
     #
     # Locals: subject, typeof, predicates, rel, element, inlist
     # Yield: predicates.each
-    :subject => %q(
+    subject: %q(
       - if element == :li
-        %li{:rel => rel, :resource => (about || resource), :typeof => typeof, :inlist => inlist}
+        %li{rel: rel, resource: (about || resource), typeof: typeof, inlist: inlist}
+          - if typeof
+            %span.type!= typeof
+          %table.properties
+            - predicates.each do |predicate|
+              != yield(predicate)
+      - elsif rel
+        %td{rel: rel, resource: (about || resource), typeof: typeof, inlist: inlist}
           - if typeof
             %span.type!= typeof
           %table.properties
             - predicates.each do |predicate|
               != yield(predicate)
       - else
-        %td{:rel => rel, :resource => (about || resource), :typeof => typeof, :inlist => inlist}
+        %div{resource: (about || resource), typeof: typeof, inlist: inlist}
           - if typeof
             %span.type!= typeof
           %table.properties
@@ -59,9 +66,9 @@ module RDF::Distiller
     # Yields: object
     # If nil is returned, render as a leaf
     # Otherwise, render result
-    :property_value => %q(
+    property_value: %q(
       - if heading_predicates.include?(predicate) && object.literal?
-        %h1{:property => get_curie(predicate), :content => get_content(object), :lang => get_lang(object), :datatype => get_dt_curie(object), :inlist => inlist}= escape_entities(get_value(object))
+        %h1{property: get_curie(predicate), content: get_content(object), lang: get_lang(object), datatype: get_dt_curie(object), inlist: inlist}= escape_entities(get_value(object))
       - else
         %tr.property
           %td.label
@@ -69,22 +76,22 @@ module RDF::Distiller
           - if res = yield(object)
             != res
           - elsif get_curie(object) == 'rdf:nil'
-            %td{:rel => get_curie(predicate), :inlist => ''}= "Empty"
+            %td{rel: get_curie(predicate), inlist: ''}= "Empty"
           - elsif object.node?
-            %td{:property => get_curie(predicate), :resource => get_curie(object), :inlist => inlist}= get_curie(object)
+            %td{property: get_curie(predicate), resource: get_curie(object), inlist: inlist}= get_curie(object)
           - elsif object.uri?
             %td
-              %a{:property => get_curie(predicate), :href => object.to_s, :inlist => inlist}= object.to_s
+              %a{property: get_curie(predicate), href: object.to_s, inlist: inlist}= object.to_s
           - elsif object.datatype == RDF.XMLLiteral
-            %td{:property => get_curie(predicate), :lang => get_lang(object), :datatype => get_dt_curie(object), :inlist => inlist}<!= get_value(object)
+            %td{property: get_curie(predicate), lang: get_lang(object), datatype: get_dt_curie(object), inlist: inlist}<!= get_value(object)
           - else
-            %td{:property => get_curie(predicate), :content => get_content(object), :lang => get_lang(object), :datatype => get_dt_curie(object), :inlist => inlist}= escape_entities(get_value(object))
+            %td{property: get_curie(predicate), content: get_content(object), lang: get_lang(object), datatype: get_dt_curie(object), inlist: inlist}= escape_entities(get_value(object))
     ),
 
     # Output for multi-valued properties
     # Locals: predicate, objects, inliste
     # Yields: object for leaf resource rendering
-    :property_values =>  %q(
+    property_values:  %q(
       %tr.property
         %td.label
           = get_predicate_name(predicate)
@@ -94,14 +101,14 @@ module RDF::Distiller
               - if res = yield(object)
                 != res
               - elsif object.node?
-                %li{:property => get_curie(predicate), :resource => get_curie(object), :inlist => inlist}= get_curie(object)
+                %li{property: get_curie(predicate), resource: get_curie(object), inlist: inlist}= get_curie(object)
               - elsif object.uri?
                 %li
-                  %a{:property => get_curie(predicate), :href => object.to_s, :inlist => inlist}= object.to_s
+                  %a{property: get_curie(predicate), href: object.to_s, inlist: inlist}= object.to_s
               - elsif object.datatype == RDF.XMLLiteral
-                %li{:property => get_curie(predicate), :lang => get_lang(object), :datatype => get_curie(object.datatype), :inlist => inlist}<!= get_value(object)
+                %li{property: get_curie(predicate), lang: get_lang(object), datatype: get_curie(object.datatype), inlist: inlist}<!= get_value(object)
               - else
-                %li{:property => get_curie(predicate), :content => get_content(object), :lang => get_lang(object), :datatype => get_dt_curie(object), :inlist => inlist}= escape_entities(get_value(object))
+                %li{property: get_curie(predicate), content: get_content(object), lang: get_lang(object), datatype: get_dt_curie(object), inlist: inlist}= escape_entities(get_value(object))
     ),
   }
 end
